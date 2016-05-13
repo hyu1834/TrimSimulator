2a95468e3b30144fcf80086b905d0aca47430a92
Hiu Hong Yu <hiuyu@ucdavis.edu>
2016-05-12 21:58:36 -0700

    started on sts

--- src/sequential_trim_simulator.cpp
+++ src/sequential_trim_simulator.cpp
@@ -9,5 +9,16 @@ Sequential_Trim_Simulator::~Sequential_Trim_Simulator()	{
 }
 
 void Sequential_Trim_Simulator::startSimulation()	{
-
+	int commandCounter = 0;
+	bool lock = false;
+	// if there are no more command to issue, end the simulation
+	while(commandCounter < commandPtr->size())	{
+		if(!lock)	{
+			
+		}
+		else	{
+			
+		}
+		clock += 0.000000001;
+	}
 }
