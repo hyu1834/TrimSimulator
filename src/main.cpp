282e41111aed8b7047eafec06b9950bf8ecd949e
hyu1834 <hiuyu@ucdavis.edu>
2016-05-19 22:42:28 -0700

    new praser

--- src/main.cpp
+++ src/main.cpp
@@ -32,17 +32,33 @@ void printHelp()	{
 				;
 }//end printHelp
 
-std::vector<Command*> procedurePraser(std::string procedureFilePath)	{
-	// vector of command based on the input file
-	std::vector<Command*> procedure;
+void praseStatistic(std::string statisticFilePath, double& readProcessTime, 
+					double& writeProcessTime, double& trimProcessTime)	{
 	// open the input file
-	std::ifstream commandFilePtr(procedureFilePath.c_str(), std::ifstream::in);
+	std::ifstream filePtr(statisticFilePath.c_str(), std::ifstream::in);
+	if(filePtr)	{
+		char commandType;
+		double time;
+		while(filePtr >> commandType >> time)	{
+			if(commandType == "T")
+				trimProcessTime = time;
+			else if(commandType == "W")
+				writeProcessTime = time;
+			else if(commandType == "R")
+				readProcessTime = time;
+		}
+	}
+}
+
+void procedurePraser(std::string procedureFilePath, std::vector<Command*>& procedure)	{
+	// open the input file
+	std::ifstream filePtr(procedureFilePath.c_str(), std::ifstream::in);
 	// if open successfully
-	if(commandFilePtr)	{
+	if(filePtr)	{
 		double issueTime;
 		char commandType;
 		// read everyline
-		while(commandFilePtr >> issueTime >> commandType)	{
+		while(filePtr >> issueTime >> commandType)	{
 			// create command based on command type
 			if(commandType == 'T')	{
 				procedure.push_back(new Trim_Command(issueTime));
@@ -52,8 +68,6 @@ std::vector<Command*> procedurePraser(std::string procedureFilePath)	{
 			}
 		}
 	}
-
-	return procedure;
 }
 
 int main(int argc, char** argv)	{
@@ -62,16 +76,20 @@ int main(int argc, char** argv)	{
 		usage_message("Trim_Simulator-"+version+"[options] <Simulation Mode>\n");
 	}
 
-	//simulation mode variables
+	//input file path variables
+	std::string statisticFilePath = "";
+	std::string procedureFilePath = "";
+
+	// simulation statistic variables
+	double readProcessTime = -1;
+	double writeProcessTime = -1;
+	double trimProcessTime = -1;
+
+	//simulation variables
+	int concurrent_command = 1;
 	bool sequential_trim=false;
 	bool queued_trim=false;
 	bool new_trim=false;
-
-	double readProcessTime = CLOCK_SPEED * 25.0,  
-		   writeProcessTime = CLOCK_SPEED * 200.0, 
-		   trimProcessTime = CLOCK_SPEED * 2000.0;
-
-	std::string procedureFilePath = "";
 
 	/*Parse the command line and load all input files*/
 	for(int i=1;i<argc;i++)	{
@@ -80,15 +98,14 @@ int main(int argc, char** argv)	{
 				printHelp();
 				return 0;
 			}//end if strcasecmp
-			else if((strcasecmp(argv[i]+1,"s")==0)|| (strcasecmp(argv[i]+1,"-sequential")==0))	{
-				sequential_trim=true;
-			}//end else if
-			else if((strcasecmp(argv[i]+1,"q")==0)|| (strcasecmp(argv[i]+1,"-queued")==0))	{
-				queued_trim=true;
-			}//end else if
-			else if((strcasecmp(argv[i]+1,"n")==0)|| (strcasecmp(argv[i]+1,"-new_trim")==0))	{
-				new_trim=true;
-			}//end else if
+			else if((strcasecmp(argv[i]+1,"s")==0)|| (strcasecmp(argv[i]+1,"-statistic")==0))	{
+				++i;
+				if(i<argc)	{
+					statisticFilePath = argv[i];
+				}//end if
+				else
+					std::cerr<<"Dangling -s or --statistic flag on command line\n";
+			}
 			else if((strcasecmp(argv[i]+1,"p")==0)|| (strcasecmp(argv[i]+1,"-procedure")==0))	{
 				++i;
 				if(i<argc)	{
@@ -97,31 +114,62 @@ int main(int argc, char** argv)	{
 				else
 					std::cerr<<"Dangling -p or --procedure flag on command line\n";
 			}
+			else if((strcasecmp(argv[i]+1,"c")==0)|| (strcasecmp(argv[i]+1,"-concurrent")==0))	{
+				++i;
+				if(i<argc)	{
+					concurrent_command = argv[i];
+				}//end if
+				else
+					std::cerr<<"Dangling -c or --concurrent flag on command line\n";
+			}
+
+			else if((strcasecmp(argv[i]+1,"sq")==0)|| (strcasecmp(argv[i]+1,"-sequential")==0))	{
+				sequential_trim=true;
+			}//end else if
+			else if((strcasecmp(argv[i]+1,"q")==0)|| (strcasecmp(argv[i]+1,"-queued")==0))	{
+				queued_trim=true;
+			}//end else if
+			else if((strcasecmp(argv[i]+1,"n")==0)|| (strcasecmp(argv[i]+1,"-new_trim")==0))	{
+				new_trim=true;
+			}//end else if
 		}
 	}//end for
 
-	//simulation process code
-	std::vector<Command*> commands;
-
-	if(procedureFilePath != "")	{
-		//prase all the procedure line
-		commands = procedurePraser(procedureFilePath);
-
-		if(sequential_trim)	{
-			Sequential_Trim_Simulator* sts = new Sequential_Trim_Simulator(commands);
-			sts->startSimulation(readProcessTime, writeProcessTime, trimProcessTime);
-		}
-
-		if(queued_trim)	{
-			Queued_Trim_Simulator* qts = new Queued_Trim_Simulator(commands);
-			qts->startSimulation(readProcessTime, writeProcessTime, trimProcessTime);
-		}
-
-		if(new_trim)	{
-
-		}
+	if(statisticFilePath == "" || procedureFilePath == "")	{
+		printHelp();
+		return -1;
 	}
 
+	//simulation process code
+	std::vector<Command*> commands;
+	praseStatistic(statisticFilePath, readProcessTime, writeProcessTime, trimProcessTime);
+	procedurePraser(procedureFilePath, commands);
+	if(readProcessTime == -1 || writeProcessTime == -1 || trimProcessTime == -1 || commands.size() < 1)	{
+		printHelp();
+		return -1;
+	}
+
+		
+
+	if(sequential_trim)	{
+		Sequential_Trim_Simulator* sts = new Sequential_Trim_Simulator(commands);
+		sts->startSimulation(readProcessTime, writeProcessTime, trimProcessTime);
+	}
+
+	if(queued_trim)	{
+		Queued_Trim_Simulator* qts = new Queued_Trim_Simulator(commands);
+		qts->startSimulation(readProcessTime, writeProcessTime, trimProcessTime);
+	}
+
+	if(new_trim)	{
+
+	}
+
+
+	/*
+		clean up
+	*/
+	// delete all command pointers
 	for(int i = 0; i < commands.size(); i++)	{
 		delete commands[i];
 	}
